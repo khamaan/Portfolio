ee14147759e288ea372210a84a2adbbe6a7b51a4
Khan Amaan <khamaan5@gmail.com>
2023-08-27 17:35:57 +0530

    hidded the mail credentials

--- home/views.py
+++ home/views.py
@@ -11,12 +11,12 @@ def home(request):
         subject = request.POST['subject']
         message = request.POST['message']
         contact = Contact.objects.create(name=name,email=email,subject=subject,message=message)
+        contact.save()
         subject = f'New contact form submission from {name}'
         sender_email = settings.EMAIL_HOST_USER  # Use the same email as in settings.py
         recipient_email = ['khamaan5@gmail.com']  # Your email where you want to receive messages
         email_body = f"Name: {name}\nEmail: {email}\nsubject: {subject}\n\nMessage:\n{message}"
-        send_mail(subject, message, email_body, sender_email, recipient_email)
-        contact.save()
+        send_mail(subject, message, email_body, sender_email, [recipient_email])
     return render(request,"index.html")
 
 def post(request):
