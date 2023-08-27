3125c26ffce5eb5b8b650d8a8437dd11baeaa0f7
Khan Amaan <khamaan5@gmail.com>
2023-08-27 18:03:07 +0530

    hidded the mail credentials

--- portfolio_webstie/config.py
+++ portfolio_webstie/config.py
@@ -2,6 +2,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465 # Use the appropriate port for your SMTP server
-EMAIL_HOST_USER = 'djangodeveloper94@example.com'
+EMAIL_HOST_USER = 'djangodeveloper94@gmail.com'
 EMAIL_HOST_PASSWORD = 'tncnzptofpmslzox'
 EMAIL_USE_SSL = True
