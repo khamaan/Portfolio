048156525a8cc9beeac9630cb63f6cca471c4c2b
Khan Amaan <khamaan5@gmail.com>
2023-08-27 16:56:58 +0530

    made feature for contact notification

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 from .models import Contact
+from django.conf import settings
 
 # Create your views here.
 def home(request):
@@ -9,6 +11,10 @@ def home(request):
         subject = request.POST['subject']
         message = request.POST['message']
         contact = Contact.objects.create(name=name,email=email,subject=subject,message=message)
+        subject = f'New contact form submission from {name}'
+        sender_email = settings.EMAIL_HOST_USER  # Use the same email as in settings.py
+        recipient_email = 'khamaan5@gmail.com'  # Your email where you want to receive messages
+        send_mail(subject, message, sender_email, [recipient_email])
         contact.save()
     return render(request,"index.html")
 
--- portfolio_webstie/settings.py
+++ portfolio_webstie/settings.py
@@ -128,3 +128,9 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'your-smtp-server.com'
+EMAIL_PORT = 587  # Use the appropriate port for your SMTP server
+EMAIL_USE_TLS = True  # Use TLS security for the connection
+EMAIL_HOST_USER = 'djangodeveloper94@example.com'
+EMAIL_HOST_PASSWORD = 'tncnzptofpmslzox'
