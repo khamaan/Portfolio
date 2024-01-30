4eb0f8f7a6405e7cd85a1ce8473aa825fd6b9684
khan Amaan <khamaan5@gmail.com>
2024-01-30 15:14:48 +0530

    Update config.py

--- portfolio_webstie/config.py
+++ portfolio_webstie/config.py
@@ -3,5 +3,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465 # Use the appropriate port for your SMTP server
 EMAIL_HOST_USER = 'djangodeveloper94@gmail.com'
-EMAIL_HOST_PASSWORD = 'tncnzptofpmslzox'
+EMAIL_HOST_PASSWORD = 'klog tyzp eumm yzzu'
 EMAIL_USE_SSL = True
