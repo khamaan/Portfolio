396f4efad8ae0c461ee7ac6e385a470ef9cdd7a1
khan Amaan <khamaan5@gmail.com>
2024-04-17 13:58:42 +0530

    solved ip address error

--- home/admin.py
+++ home/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['name','subject','email']
+    list_display = ['name','subject','email', "ip_address"]
 
 admin.site.register(Contact,ContactAdmin)
--- home/models.py
+++ home/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=20)
     subject = models.CharField(max_length=30)
     email = models.EmailField()
     message = models.TextField()
+    ip_address = models.GenericIPAddressField(null=True, blank=True)  # Make the field nullable
     
-    def __str__ (self):
-       return self.name
-    
+    def __str__(self):
+        return self.name
